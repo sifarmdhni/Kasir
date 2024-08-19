1f2374f1628fce1d191071d9d4526841f6f45d32
fauzan774 <fauzanganteng41@gmail.com>
2024-08-19 11:06:44 +0700

    menambahkan customer

--- database/migrations/2024_08_19_032615_create_customer_table.php
+++ database/migrations/2024_08_19_032615_create_customer_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('customer', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->string('no_telpon', 100);
-            $table->string('name', 100);
-            $table->timestamps();
+            $table->string('name');
+            $table->string('no_telpon');
+            $table->string('email')->unique();
+            $table->string('diskon');
+            $table->timestamps('created_at')->nullable();
         });
     }
 
